301e5437ad1364dbae347f82debf94d241d790ce
Semol <semolmahamud2233@gmail.com>
2020-09-20 23:37:12 +0600

    dah

--- roaltv_api/settings.py
+++ roaltv_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm61-)!csjs(*2-dv_@w2eewgkos^k-1ma@vwdq5d6xw$$_6a5m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
