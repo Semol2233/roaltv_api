868fc575472b752c00b493d0736709736d69b0db
Semol <semolmahamud2233@gmail.com>
2020-09-21 13:14:20 +0600

    test1a1

--- roaltv_api/settings.py
+++ roaltv_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm61-)!csjs(*2-dv_@w2eewgkos^k-1ma@vwdq5d6xw$$_6a5m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['test.reolib.com']
 
 
 # Application definition
