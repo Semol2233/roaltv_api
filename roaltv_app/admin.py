c87de7de12a8b35c7a522c11ee8c20cee480702f
Semol <semolmahamud2233@gmail.com>
2020-10-09 23:13:50 +0600

    jdhv

--- roaltv_app/admin.py
+++ roaltv_app/admin.py
@@ -2,18 +2,21 @@ from django.contrib import admin
 
 admin.site.site_header = "RoalTv"
 
-
-class Post_modelsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'catgory','release_date')
-    list_filter = ('mfg_date', )
+class post_modelsAdmin(admin.ModelAdmin):
+    list_display = ('id','title','release_date')
+    list_filter = ('title', )
     search_fields = ("title__startswith", )
 
+    
+
 from .models import *
-admin.site.register(post_models)
+admin.site.register(post_models ,post_modelsAdmin)
 admin.site.register(catgory_list)
 admin.site.register(youtube_videoplaylist)
 admin.site.register(movie_pageCover)
 admin.site.register(livetvlink)
 admin.site.register(about)
+
+
 
 
