12d248639b2684b526eccfe0848de04df020aac1
Semol <semolmahamud2233@gmail.com>
2020-10-14 21:35:37 +0600

    tets

--- roaltv_app/admin.py
+++ roaltv_app/admin.py
@@ -10,12 +10,12 @@ class post_modelsAdmin(admin.ModelAdmin):
     
 
 from .models import *
-admin.site.register(post_models ,post_modelsAdmin)
-admin.site.register(catgory_list)
-admin.site.register(youtube_videoplaylist)
-admin.site.register(movie_pageCover)
-admin.site.register(livetvlink)
-admin.site.register(about)
+# admin.site.register(post_models ,post_modelsAdmin)
+# admin.site.register(catgory_list)
+# admin.site.register(youtube_videoplaylist)
+# admin.site.register(movie_pageCover)
+# admin.site.register(livetvlink)
+# admin.site.register(about)
 admin.site.register(Profile)
 admin.site.register(apps_coverimg)
 admin.site.register(livetvlist)
